73cfaa1d38ead33163cfaee3aa5c9d8baf120af0
Kazuki Inamura <kzinmr109@gmail.com>
2019-10-14 17:35:20 +0900

    Revert "fix: flake8"
    This reverts commit 51b89c404b9f498d8886e7d71f5d13d57ce0e9ba.

--- backend/models.py
+++ backend/models.py
@@ -10,24 +10,22 @@ database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 db = SQLAlchemy()
 
 
-
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
 def setup_db(app, database_path=database_path):
-    '''
-    setup_db(app)
-        binds a flask application and a SQLAlchemy service
-    '''
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
+'''
+Question
 
-class Question(db.Model):
-    '''
-    Question
-
-    '''
+'''
+class Question(db.Model):  
   __tablename__ = 'questions'
 
   id = Column(Integer, primary_key=True)
@@ -62,12 +60,11 @@ class Question(db.Model):
       'difficulty': self.difficulty
     }
 
+'''
+Category
 
-class Category(db.Model):
-    '''
-    Category
-
-    '''
+'''
+class Category(db.Model):  
   __tablename__ = 'categories'
 
   id = Column(Integer, primary_key=True)
